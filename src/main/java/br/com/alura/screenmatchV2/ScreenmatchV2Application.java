9c0bd9200cdd936475dd8aa5419a2d47da426e88
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-09-16 20:25:15 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/alura/screenmatchV2/ScreenmatchV2Application.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatchV2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchV2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchV2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/br/com/alura/screenmatchV2/ScreenmatchV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
