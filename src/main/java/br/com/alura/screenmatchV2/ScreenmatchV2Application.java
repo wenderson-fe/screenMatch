370bda1757ea7633baabc3c99fc4d89419349f47
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2024-09-24 14:26:42 -0300

    Criando o menu do usuário

--- src/main/java/br/com/alura/screenmatchV2/ScreenmatchV2Application.java
+++ src/main/java/br/com/alura/screenmatchV2/ScreenmatchV2Application.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenmatchV2;
 
-import br.com.alura.screenmatchV2.model.DadosEpisidodio;
-import br.com.alura.screenmatchV2.model.DadosSerie;
-import br.com.alura.screenmatchV2.model.DadosTemporada;
-import br.com.alura.screenmatchV2.service.ConsumoApi;
-import br.com.alura.screenmatchV2.service.ConverteDados;
+import br.com.alura.screenmatchV2.main.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchV2Application implements CommandLineRunner {
@@ -21,26 +14,7 @@ public class ScreenmatchV2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=dd235501");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=dd235501");
-		DadosEpisidodio dadosEpisidodio = conversor.obterDados(json, DadosEpisidodio.class);
-		System.out.println(dadosEpisidodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=dd235501");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatchV2/main/Principal.java
@@ -0,0 +1,41 @@
+package br.com.alura.screenmatchV2.main;
+
+import br.com.alura.screenmatchV2.model.DadosSerie;
+import br.com.alura.screenmatchV2.model.DadosTemporada;
+import br.com.alura.screenmatchV2.service.ConsumoApi;
+import br.com.alura.screenmatchV2.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private Scanner sc = new Scanner(System.in);
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=dd235501";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para busca: ");
+        var nomeSerie = sc.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
+    }
+
+
+}
